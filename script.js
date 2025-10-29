b754b8350b9e47509c9e92e4237b4f16b7c54aab
arun980798 <arunrajput88176894@gmail.com>
2025-10-29 23:31:01 +0530

    task 1 complet js

--- script.js
+++ script.js
@@ -1,135 +1,157 @@
-function aleAndprom() {
+// function aleAndprom() {
 
-  alert("it chnage the thngs and mae the new string")
-  let a = prompt("hello   ");
-  let b = a.slice(1, a.length);
-  console.log(b);
+//   alert("it chnage the thngs and mae the new string")
+//   let a = prompt("hello   ");
+//   let b = a.slice(1, a.length);
+//   console.log(b);
 
-}
-// aleAndprom()
-function vario() {
+// }
+// // aleAndprom()
+// function vario() {
 
-  let a;
-  a = 12;
-  console.log(a)
-
-
-  const b = 12;
-  console.info(b);
-}
-// vario()
-function consol() {
-
-  let fruits = ["Apple", "Banana", "Mango", "Orange"];
-
-  let person = {
-    name: "Arun",
-    age: 20,
-    city: "Delhi",
-    isStudent: true
-  };
-
-  let a = "this is not your thing ";
-
-  console.log(a);
-  console.info(a);
-  console.warn(a);
-  console.error(a);
-  console.table(fruits)
-  console.table(person)
+//   let a;
+//   a = 12;
+//   console.log(a)
 
 
-}
-// consol()
-function astr() {
-  let a = "string  hai ye ";
-  let t = `tamplet litrel ${2 + 3} we can do write js code in the string `;
-  console.info(a)
-  console.info(t)
+//   const b = 12;
+//   console.info(b);
+// }
+// // vario()
+// function consol() {
 
-}
+//   let fruits = ["Apple", "Banana", "Mango", "Orange"];
 
-// astr();
+//   let person = {
+//     name: "Arun",
+//     age: 20,
+//     city: "Delhi",
+//     isStudent: true
+//   };
 
+//   let a = "this is not your thing ";
 
-function afunmethod() {
-  let text = "Hello World JavaScript";
-  // split who saprate words and things from string 
-  {
-    {
-      let words = text.split(" ");
-      console.log(words);
-      // this is saptrate word by the space
-    }
-    {
-      let w2 = text.split("")
-      console.log(w2)
-      // ye single singe words ko saptate kar leta h or space hata deta h 
-    }
-    {
-      let w3 = text.split('o')
-      console.info(w3)
-      // this will split the word by thrir letter 
-
-    }
-  }
-
-  // replace
-  {
-    let r1= text.replace("World","dosto")
-    console.log(r1)
-    // this will cahnge the wold to dosto
-
-  }
-  // include
-  {
-    let i1= text.includes("l")
-    console.log(i1)
-    // this will check this word are present in this or not 
-
-  }
+//   console.log(a);
+//   console.info(a);
+//   console.warn(a);
+//   console.error(a);
+//   console.table(fruits)
+//   console.table(person)
 
 
-}
+// }
+// // consol()
+// function astr() {
+//   let a = "string  hai ye ";
+//   let t = `tamplet litrel ${2 + 3} we can do write js code in the string `;
+//   console.info(a)
+//   console.info(t)
 
-// afunmethod()
+// }
+
+// // astr();
+
+
+// function afunmethod() {
+//   let text = "Hello World JavaScript";
+//   // split who saprate words and things from string 
+//   {
+//     {
+//       let words = text.split(" ");
+//       console.log(words);
+//       // this is saptrate word by the space
+//     }
+//     {
+//       let w2 = text.split("")
+//       console.log(w2)
+//       // ye single singe words ko saptate kar leta h or space hata deta h 
+//     }
+//     {
+//       let w3 = text.split('o')
+//       console.info(w3)
+//       // this will split the word by thrir letter 
+
+//     }
+//   }
+
+//   // replace
+//   {
+//     let r1= text.replace("World","dosto")
+//     console.log(r1)
+//     // this will cahnge the wold to dosto
+
+//   }
+//   // include
+//   {
+//     let i1= text.includes("l")
+//     console.log(i1)
+//     // this will check this word are present in this or not 
+
+//   }
+
+
+// }
+
+// // afunmethod()
 
 
 
-function taskday3(){
-  // alert("hello sir ");
-  // prompt("write your name ");
-  // let a =  prompt("or batao kay hal");
-  // console.log(2+2);
+// function taskday3(){
+//   // alert("hello sir ");
+//   // prompt("write your name ");
+//   // let a =  prompt("or batao kay hal");
+//   // console.log(2+2);
 
-  // let city = "bhopal"
-  // console.info(city)
+//   // let city = "bhopal"
+//   // console.info(city)
 
-  var na = "arun ";
-  let b ="rajput";
-  const  c="mur";
+//   var na = "arun ";
+//   let b ="rajput";
+//   const  c="mur";
 
-  na="arn";
-  b= "jjjidsdj";
-  // const me possible nai h 
-  // agar const me kare h to vo brakuet ke bhar use nai hoga or dusre koi jhaga use karoge to vo {} inke andar he kam karene nai to bhar error ayega or var me asa nai h vo function ke andar kahi be kam kar dega or re assine be ho jayega 
-  const ownerOfThisWeb= "arun"
-  const valueofpai= "3.14"
+//   na="arn";
+//   b= "jjjidsdj";
+//   // const me possible nai h 
+//   // agar const me kare h to vo brakuet ke bhar use nai hoga or dusre koi jhaga use karoge to vo {} inke andar he kam karene nai to bhar error ayega or var me asa nai h vo function ke andar kahi be kam kar dega or re assine be ho jayega 
+//   const ownerOfThisWeb= "arun"
+//   const valueofpai= "3.14"
 
-  let a = prompt("your namae ")
-  alert(`welcome  ${a}`)
+//   let a = prompt("your namae ")
+//   alert(`welcome  ${a}`)
 
-  console.log(typeof(a));
-  console.log(`yourage ${a+5}`);
-  let y ;
-  console.log(y)
+//   console.log(typeof(a));
+//   console.log(`yourage ${a+5}`);
+//   let y ;
+//   console.log(y)
 
 
   
 
 
 
-}
+// }
 
-taskday3()
+// // taskday3()
 
+
+
+
+
+
+
+
+// 5+10
+// let a = 10;
+// let x ;
+// console.log( typeof x);
+
+
+// // null is given when you want to give value but now you dont have value now 
+// // now undefine is use when you dont know you give this vslue or not
+
+
+
+
+
+
+console.log(12%13)
